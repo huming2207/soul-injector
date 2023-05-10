71f7c7f33f98c6927b6feffa05b812e2b4fa65e3
Jackson Ming Hu <huming2207@gmail.com>
2023-05-10 22:59:01 +1000

    file_util: make a helper function to calculate CRC32 and output it instead of compare only

--- main/misc/includes/file_utils.hpp
+++ main/misc/includes/file_utils.hpp
@@ -11,14 +11,39 @@ private:
 public:
     static esp_err_t validate_file_crc32(const char *path, uint32_t crc, size_t len = 0)
     {
+        uint32_t actual_crc = 0;
+        auto ret = get_file_crc32(path, &actual_crc, len);
+        if (ret != ESP_OK) {
+            return ret;
+        }
+
+        if (actual_crc != crc) {
+            ESP_LOGE(TAG, "CRC mismatch for firmware %s, expected 0x%lx, actual 0x%lx", path, crc, actual_crc);
+            return ESP_ERR_INVALID_CRC;
+        } else {
+            ESP_LOGI(TAG, "CRC matched for firmware %s, expected 0x%lx, actual 0x%lx", path, crc, actual_crc);
+        }
+
+        return ESP_OK;
+    }
+
+    static esp_err_t get_file_crc32(const char *path, uint32_t *crc_out, size_t len = 0)
+    {
         if (path == nullptr) {
             ESP_LOGE(TAG, "Path is null!");
             return ESP_ERR_INVALID_ARG;
         }
 
+        if (crc_out == nullptr) {
+            ESP_LOGE(TAG, "CRC output is null!");
+            return ESP_ERR_INVALID_ARG;
+        }
+
+        // Checking errno is not thread safe, so we do a fopen to test the existence of this file
+        // access() is not an option sometimes, e.g. LittleFS wrapper doesn't implement this function
         FILE *file = fopen(path, "rb");
         if (file == nullptr) {
-            ESP_LOGE(TAG, "Failed when reading firmware file");
+            ESP_LOGE(TAG, "Failed when opening file: %s", path);
             return ESP_ERR_NOT_FOUND;
         }
 
@@ -32,25 +57,18 @@ public:
             }
 
             fseek(file, 0, SEEK_SET);
-
         }
 
         uint32_t actual_crc = 0;
         while(len > 0) {
-            uint8_t buf[1024] = { 0 };
+            uint8_t buf[256] = { 0 };
             auto read_len = std::min(len, sizeof(buf));
             auto actual_read = fread(buf, 1, read_len, file);
             actual_crc = esp_crc32_le(actual_crc, buf, actual_read);
             len -= actual_read;
         }
 
-        if (actual_crc != crc) {
-            ESP_LOGE(TAG, "CRC mismatch for firmware %s, expected 0x%lx, actual 0x%lx", path, crc, actual_crc);
-            return ESP_ERR_INVALID_CRC;
-        } else {
-            ESP_LOGI(TAG, "CRC matched for firmware %s, expected 0x%lx, actual 0x%lx", path, crc, actual_crc);
-        }
-
+        *crc_out = actual_crc;
         return ESP_OK;
     }
 };

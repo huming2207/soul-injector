d611c99672d9a56f8a484781f956a3bea44258cc
Jackson Ming Hu <huming2207@gmail.com>
2023-06-15 22:52:03 +1000

    file_utils: implement file R/W & get length

--- main/misc/includes/file_utils.hpp
+++ main/misc/includes/file_utils.hpp
@@ -50,12 +50,6 @@ public:
         if (len < 1) {
             fseek(file, 0, SEEK_END);
             len = ftell(file);
-            if (len < 4 || len % 4 != 0) {
-                ESP_LOGE(TAG, "Manifest in a wrong length: %u", len);
-                fclose(file);
-                return ESP_ERR_INVALID_STATE;
-            }
-
             fseek(file, 0, SEEK_SET);
         }
 
@@ -69,6 +63,115 @@ public:
         }
 
         *crc_out = actual_crc;
+        return ESP_OK;
+    }
+
+    static esp_err_t get_len(const char *path, size_t *actual_len)
+    {
+        if (path == nullptr || actual_len == nullptr) {
+            ESP_LOGE(TAG, "Param is null!");
+            return ESP_ERR_INVALID_ARG;
+        }
+
+        FILE *file = fopen(path, "rb");
+        if (file == nullptr) {
+            ESP_LOGE(TAG, "Failed when opening file: %s", path);
+            return ESP_ERR_NOT_FOUND;
+        }
+
+        fseek(file, 0, SEEK_END);
+        size_t len = ftell(file);
+        if (len < 4 || len % 4 != 0) {
+            ESP_LOGE(TAG, "Manifest in a wrong length: %u", len);
+            fclose(file);
+            return ESP_ERR_INVALID_STATE;
+        }
+
+        fclose(file);
+        *actual_len = len;
+        return ESP_OK;
+    }
+
+    static esp_err_t read_to_buf(const char *path, uint8_t *buf_out, size_t buf_len, size_t *actual_len = nullptr)
+    {
+        if (path == nullptr) {
+            ESP_LOGE(TAG, "Path is null!");
+            return ESP_ERR_INVALID_ARG;
+        }
+
+        if (buf_out == nullptr) {
+            ESP_LOGE(TAG, "Buffer is null!");
+            return ESP_ERR_INVALID_ARG;
+        }
+
+        // Checking errno is not thread safe, so we do a fopen to test the existence of this file
+        // access() is not an option sometimes, e.g. LittleFS wrapper doesn't implement this function
+        FILE *file = fopen(path, "rb");
+        if (file == nullptr) {
+            ESP_LOGE(TAG, "Failed when opening file: %s", path);
+            return ESP_ERR_NOT_FOUND;
+        }
+
+        fseek(file, 0, SEEK_END);
+        size_t len = ftell(file);
+        if (len < 4 || len % 4 != 0) {
+            ESP_LOGE(TAG, "Manifest in a wrong length: %u", len);
+            fclose(file);
+            return ESP_ERR_INVALID_STATE;
+        }
+
+        fseek(file, 0, SEEK_SET);
+        if (len > buf_len) {
+            ESP_LOGE(TAG, "File too big: buffer %p size %u, expect %u", buf_out, buf_len, len);
+            return ESP_ERR_INVALID_SIZE;
+        } else {
+            if (actual_len != nullptr) {
+                *actual_len = len;
+            }
+        }
+
+        size_t read_pos = 0;
+        while(len > 0) {
+            uint8_t buf[256] = { 0 };
+            auto read_len = std::min(len, sizeof(buf));
+            auto actual_read = fread(buf, 1, read_len, file);
+
+            memcpy(buf_out + read_pos, buf, actual_read);
+            len -= actual_read;
+            read_pos += actual_read;
+        }
+
+        fclose(file);
+        return ESP_OK;
+    }
+
+    static esp_err_t write_to_file(const char *path, const uint8_t *buf, size_t len)
+    {
+        if (path == nullptr) {
+            ESP_LOGE(TAG, "Path is null!");
+            return ESP_ERR_INVALID_ARG;
+        }
+
+        if (buf == nullptr || len < 1) {
+            ESP_LOGE(TAG, "Buffer is null!");
+            return ESP_ERR_INVALID_ARG;
+        }
+
+        // Checking errno is not thread safe, so we do a fopen to test the existence of this file
+        // access() is not an option sometimes, e.g. LittleFS wrapper doesn't implement this function
+        FILE *file = fopen(path, "w");
+        if (file == nullptr) {
+            ESP_LOGE(TAG, "Failed when opening file: %s", path);
+            return ESP_ERR_NOT_FOUND;
+        }
+
+        if (fwrite(buf, len, 1, file) < 1) {
+            ESP_LOGE(TAG, "Write failed for %s from buf %p len %u", path, buf, len);
+            return ESP_FAIL;
+        }
+
+        fflush(file);
+        fclose(file);
         return ESP_OK;
     }
 };

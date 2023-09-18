497e5dc6811428cdb878cc0dc4ea8d4e3c51d668
Jackson Ming Hu <huming2207@gmail.com>
2023-09-18 22:58:37 +1000

    display_manager: implement some init sequence

--- main/driver/lcd/display_manager.cpp
+++ main/driver/lcd/display_manager.cpp
@@ -1,5 +1,53 @@
-//
-// Created by hu on 9/09/23.
-//
-
+#include <esp_log.h>
 #include "display_manager.hpp"
+
+esp_err_t display_manager::init()
+{
+    auto ret = panel->init();
+    if (ret != ESP_OK) {
+        return ret;
+    }
+
+    disp_buf_a = static_cast<uint8_t *>(heap_caps_malloc(CONFIG_SI_DISP_PANEL_BUFFER_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA));
+    if (disp_buf_a == nullptr) {
+        ESP_LOGE(TAG, "Failed to allocate display buffer");
+        return ESP_ERR_NO_MEM;
+    }
+
+    disp_buf_b = static_cast<uint8_t *>(heap_caps_malloc(CONFIG_SI_DISP_PANEL_BUFFER_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA));
+    if (disp_buf_b == nullptr) {
+        ESP_LOGE(TAG, "Failed to allocate display buffer");
+        free(disp_buf_a);
+        return ESP_ERR_NO_MEM;
+    }
+
+    lv_disp_draw_buf_init(&draw_buf, disp_buf_a, disp_buf_b, CONFIG_SI_DISP_PANEL_BUFFER_SIZE);
+    static lv_disp_drv_t disp_drv = {};
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.flush_cb = handle_flush;
+    disp_drv.hor_res = (int16_t)panel->get_hor_size();
+    disp_drv.ver_res = (int16_t)panel->get_ver_size();
+    disp_drv.draw_buf = &draw_buf;
+    disp_drv.antialiasing = 1;
+
+    lv_disp_drv_register(&disp_drv);
+
+    return ret;
+
+}
+
+disp_panel_if *display_manager::get_panel()
+{
+    return panel;
+}
+
+void display_manager::handle_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p)
+{
+
+}
+
+void IRAM_ATTR display_manager::lv_tick_cb(void *arg)
+{
+    (void) arg;
+    lv_tick_inc(1);
+}
--- main/driver/lcd/display_manager.hpp
+++ main/driver/lcd/display_manager.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+#include <freertos/task.h>
 #include <esp_err.h>
 #include "lvgl.h"
+#include "nfp190b_panel.hpp"
 
 class disp_panel_if
 {
@@ -10,6 +14,8 @@ public:
     virtual esp_err_t set_backlight(uint8_t level) = 0;
     virtual void flush_display(lv_disp_drv_t *disp_drv, const lv_area_t * area, lv_color_t * color_p) = 0;
     virtual esp_err_t deinit() = 0;
+    [[nodiscard]] virtual size_t get_hor_size() const = 0;
+    [[nodiscard]] virtual size_t get_ver_size() const = 0;
 };
 
 class display_manager
@@ -28,5 +34,23 @@ public:
     disp_panel_if *get_panel();
 
 private:
-    disp_panel_if *panel;
+    static void handle_flush(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p);
+    static void IRAM_ATTR lv_tick_cb(void *arg);
+
+private:
+#ifdef CONFIG_SI_DISP_PANEL_NFP190B
+    disp_panel_if *panel = (disp_panel_if *)(new nfp190b_panel());
+#elif defined(CONFIG_SI_DISP_PANEL_LHS154KC)
+    isp_panel_if *panel = (disp_panel_if *)(new nfp190b_panel());
+#endif
+
+private:
+    static const constexpr char TAG[] = "disp_mgr";
+    lv_disp_draw_buf_t draw_buf = {};
+    uint8_t *disp_buf_a = nullptr;
+    uint8_t *disp_buf_b = nullptr;
+    SemaphoreHandle_t lv_ui_task_lock = nullptr;
+    StaticTask_t lv_ui_task_stack = {};
+    StackType_t *lv_ui_task_stack_buf = nullptr;
+    TaskHandle_t lv_ui_task_handle = nullptr;
 };

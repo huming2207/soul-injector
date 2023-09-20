04d1d449ef6b81e6dd2a5800e70be7b3c95ff4c0
Jackson Ming Hu <huming2207@gmail.com>
2023-09-20 16:38:49 +1000

    flashalgo_parser: implement parsing logic

--- main/prog/flash_algo_parser.cpp
+++ main/prog/flash_algo_parser.cpp
@@ -1,1 +1,75 @@
+#include <esp_log.h>
+#include <elfio/elfio_dump.hpp>
 #include "flash_algo_parser.hpp"
+
+esp_err_t flash_algo_parser::load(const char *path)
+{
+    if (!elf_parser.load(path)) {
+        ESP_LOGE(TAG, "ELF parse failed!");
+        return ESP_FAIL;
+    }
+
+    ESP_LOGI(TAG, "ELF loaded, version=%s, type=%s",
+             ELFIO::dump::str_version(elf_parser.get_version()).c_str(),
+             ELFIO::dump::str_type(elf_parser.get_type()).c_str()
+    );
+
+    ESP_LOGI(TAG, "ELF class=%s, OS_ABI=%s, Machine=%s",
+             ELFIO::dump::str_class(elf_parser.get_class()).c_str(),
+             ELFIO::dump::str_os_abi(elf_parser.get_os_abi()).c_str(),
+             ELFIO::dump::str_machine(elf_parser.get_machine()).c_str()
+    );
+
+    // We probably need to enforce a check here (e.g. is it ARM or RISCV? is it a bare-metal ELF?)
+    return ESP_OK;
+}
+
+esp_err_t flash_algo_parser::get_test_description(flash_algo::test_description *descr)
+{
+    return get_section_data(descr, "SelfTestInfo", sizeof(flash_algo::test_description), nullptr);
+}
+
+esp_err_t flash_algo_parser::get_dev_description(flash_algo::dev_description *descr)
+{
+    return get_section_data(descr, "DeviceData", sizeof(flash_algo::dev_description), nullptr);
+}
+
+esp_err_t flash_algo_parser::get_flash_algo(uint8_t *buf_out, size_t buf_len, size_t *actual_len)
+{
+    return get_section_data(buf_out, "PrgCode", buf_len, actual_len);
+}
+
+esp_err_t flash_algo_parser::get_section_data(void *data_out, const char *section_name, size_t min_size, size_t *actual_size) const
+{
+    if (data_out == nullptr) {
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    auto section_cnt = elf_parser.sections.size();
+    if (section_cnt < 1) {
+        ESP_LOGE(TAG, "No section at all?");
+        return ESP_ERR_INVALID_SIZE;
+    }
+
+    for (size_t idx = 0; idx < section_cnt; idx += 1) {
+        auto curr_section = elf_parser.sections[idx];
+        if ( curr_section->get_type() == ELFIO::SHT_NOBITS ) {
+            continue;
+        }
+
+        if (curr_section->get_name() == section_name) {
+            memcpy(data_out, curr_section->get_data(), std::min((size_t)curr_section->get_size(), min_size));
+            if (actual_size != nullptr) {
+                *actual_size = curr_section->get_size();
+            }
+
+            return ESP_OK;
+        } else {
+            continue;
+        }
+    }
+
+    ESP_LOGE(TAG, "Section '%s' not found!", section_name);
+    return ESP_ERR_NOT_FOUND;
+}
+
--- main/prog/includes/flash_algo_parser.hpp
+++ main/prog/includes/flash_algo_parser.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstdint>
+#include <esp_err.h>
+#include <elfio/elfio.hpp>
 
 namespace flash_algo
 {
@@ -22,7 +24,7 @@ namespace flash_algo
         uint8_t empty;
         uint32_t prog_timeout;
         uint32_t erase_timeout;
-        flash_sector *sectors;
+        flash_sector sectors[];
     };
 
     struct __attribute__((packed)) test_item
@@ -37,7 +39,7 @@ namespace flash_algo
         uint32_t ram_start_addr;
         uint32_t ram_end_addr;
         uint32_t test_cnt;
-        test_item *test_items;
+        test_item test_items[];
     };
 
     static const constexpr uint32_t SELF_TEST_MAGIC = 0x536f756c; // "Soul"
@@ -45,5 +47,18 @@ namespace flash_algo
 
 class flash_algo_parser
 {
+public:
+    esp_err_t load(const char *path);
+    esp_err_t get_test_description(flash_algo::test_description *descr);
+    esp_err_t get_dev_description(flash_algo::dev_description *descr);
+    esp_err_t get_flash_algo(uint8_t *buf_out, size_t buf_len, size_t *actual_len);
 
+private:
+    esp_err_t get_section_data(void *data_out, const char *section_name, size_t min_size, size_t *actual_size) const;
+
+private:
+    ELFIO::elfio elf_parser;
+
+private:
+    static const constexpr char TAG[] = "algo_parser";
 };

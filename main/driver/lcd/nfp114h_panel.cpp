0133faac1009df19d6b9149dba6894ab8b6edef1
Jackson Ming Hu <huming2207@gmail.com>
2023-10-11 23:06:21 +1100

    lcd: nfp114h: init

---
+++ main/driver/lcd/nfp114h_panel.cpp
@@ -0,0 +1,139 @@
+#include <driver/gpio.h>
+#include <esp_log.h>
+#include <driver/spi_master.h>
+#include <esp_lcd_panel_vendor.h>
+#include <esp_lcd_panel_ops.h>
+
+#include "nfp114h_panel.hpp"
+
+esp_err_t nfp114h_panel::init()
+{
+    gpio_config_t pwr_io_cfg = {
+            .pin_bit_mask = (1ULL << CONFIG_SI_DISP_PANEL_BKL),
+            .mode = GPIO_MODE_OUTPUT,
+            .pull_up_en = GPIO_PULLUP_ENABLE,
+            .pull_down_en = GPIO_PULLDOWN_DISABLE,
+            .intr_type = GPIO_INTR_DISABLE,
+    };
+
+    auto ret = gpio_config(&pwr_io_cfg);
+    ret = ret ?: gpio_reset_pin((gpio_num_t)CONFIG_SI_DISP_PANEL_IO_CS);
+    ret = ret ?: gpio_reset_pin((gpio_num_t)CONFIG_SI_DISP_PANEL_IO_DC);
+    ret = ret ?: gpio_reset_pin((gpio_num_t)CONFIG_SI_DISP_PANEL_IO_MOSI);
+    ret = ret ?: gpio_reset_pin((gpio_num_t)CONFIG_SI_DISP_PANEL_IO_RST);
+    ret = ret ?: gpio_reset_pin((gpio_num_t)CONFIG_SI_DISP_PANEL_IO_SCLK);
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "GPIO setup failed: 0x%x", ret);
+        return ret;
+    }
+
+    spi_bus_config_t spi_bus_cfg = {};
+    spi_bus_cfg.sclk_io_num = CONFIG_SI_DISP_PANEL_IO_SCLK;
+    spi_bus_cfg.mosi_io_num = CONFIG_SI_DISP_PANEL_IO_MOSI;
+    spi_bus_cfg.miso_io_num = GPIO_NUM_NC;
+    spi_bus_cfg.quadhd_io_num = GPIO_NUM_NC;
+    spi_bus_cfg.quadwp_io_num = GPIO_NUM_NC;
+    ret = spi_bus_initialize(LCD_SPI_HOST, &spi_bus_cfg, SPI_DMA_CH_AUTO);
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "SPI Bus init failed: 0x%x", ret);
+        return ret;
+    }
+
+    esp_lcd_panel_io_spi_config_t io_cfg = {};
+    io_cfg.dc_gpio_num = CONFIG_SI_DISP_PANEL_IO_DC;
+    io_cfg.cs_gpio_num = CONFIG_SI_DISP_PANEL_IO_CS;
+
+#ifndef CONFIG_SI_DISP_SLOW_CLK
+    io_cfg.pclk_hz = SPI_MASTER_FREQ_20M;
+#else
+    io_cfg.pclk_hz = SPI_MASTER_FREQ_8M;
+#endif
+
+    io_cfg.lcd_cmd_bits = 8;
+    io_cfg.lcd_param_bits = 8;
+    io_cfg.spi_mode = 0;
+    io_cfg.trans_queue_depth = 10;
+    io_cfg.on_color_trans_done = handle_fb_trans_finish;
+    io_cfg.user_ctx = this;
+    ret = esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_SPI_HOST, &io_cfg, &io_handle);
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "SPI dev init failed: 0x%x", ret);
+        return ret;
+    }
+
+    esp_lcd_panel_dev_config_t panel_cfg = {};
+    panel_cfg.reset_gpio_num = CONFIG_SI_DISP_PANEL_IO_RST;
+    panel_cfg.rgb_ele_order = LCD_RGB_ELEMENT_ORDER_BGR;
+    panel_cfg.bits_per_pixel = 16;
+    ret = esp_lcd_new_panel_st7789(io_handle, &panel_cfg, &panel_handle);
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Panel create failed: 0x%x", ret);
+        return ret;
+    }
+
+    ret = set_backlight(0);
+    ret = ret ?: esp_lcd_panel_reset(panel_handle);
+    ret = ret ?: esp_lcd_panel_init(panel_handle);
+    ret = ret ?: esp_lcd_panel_invert_color(panel_handle, true);
+    ret = ret ?: esp_lcd_panel_swap_xy(panel_handle, true);
+    ret = ret ?: esp_lcd_panel_set_gap(panel_handle, 52, 40); // This is probably wrong - try 40, 53 and 52 combos
+    ret = ret ?: send_sequence(LCD_INIT_SEQ, sizeof(LCD_INIT_SEQ) / sizeof(nfp114h::seq_t));
+
+
+    return ret;
+}
+
+esp_err_t nfp114h_panel::set_backlight(uint8_t level)
+{
+    return ESP_OK;
+}
+
+void nfp114h_panel::flush_display(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p)
+{
+
+}
+
+esp_err_t nfp114h_panel::deinit()
+{
+    return 0;
+}
+
+size_t nfp114h_panel::get_hor_size() const
+{
+    return 0;
+}
+
+size_t nfp114h_panel::get_ver_size() const
+{
+    return 0;
+}
+
+lv_disp_drv_t *nfp114h_panel::get_lv_disp_drv()
+{
+    return nullptr;
+}
+
+esp_err_t nfp114h_panel::setup_lvgl(lv_disp_draw_buf_t *draw_buf)
+{
+    return 0;
+}
+
+bool nfp114h_panel::handle_fb_trans_finish(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
+{
+    return false;
+}
+
+esp_err_t nfp114h_panel::send_sequence(const nfp114h::seq_t *seq, size_t seq_cnt)
+{
+    esp_err_t ret = ESP_OK;
+
+    for (size_t idx = 0; idx < seq_cnt; idx += 1) {
+        ret = ret ?: esp_lcd_panel_io_tx_param(io_handle, seq[idx].reg, (seq[idx].len > 0 ? seq[idx].data : nullptr), seq[idx].len);
+    }
+
+    return ret;
+}
---
+++ main/driver/lcd/nfp114h_panel.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <esp_lcd_panel_io.h>
+#include <lvgl.h>
+
+#include "disp_panel_if.hpp"
+
+namespace nfp114h
+{
+    typedef struct {
+        uint8_t reg;
+        uint8_t data[16];
+        uint8_t len;
+    } seq_t;
+}
+
+class nfp114h_panel : public disp_panel_if
+{
+public:
+    esp_err_t init() override;
+    esp_err_t set_backlight(uint8_t level) override;
+    void flush_display(lv_disp_drv_t *disp_drv, const lv_area_t * area, lv_color_t * color_p) override;
+    esp_err_t deinit() override;
+    [[nodiscard]] size_t get_hor_size() const override;
+    [[nodiscard]] size_t get_ver_size() const override;
+    lv_disp_drv_t *get_lv_disp_drv() override;
+    esp_err_t setup_lvgl(lv_disp_draw_buf_t *draw_buf) override;
+
+private:
+    static bool handle_fb_trans_finish(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx);
+    esp_err_t send_sequence(const nfp114h::seq_t *seq, size_t seq_cnt);
+
+private:
+    static const constexpr char TAG[] = "nfp114h_drv";
+    static const constexpr spi_host_device_t LCD_SPI_HOST = SPI2_HOST;
+    static const constexpr nfp114h::seq_t LCD_INIT_SEQ[] = { // Black magic from ZJY
+            {0x3a, {0x05},1},
+            {0xb2, {0x0c, 0x0c, 0x00, 0x33, 0x33}, 5},
+            {0xb7, {0x35}, 1},
+            {0xbb, {0x19}, 1},
+            {0xc0, {0x2c}, 1},
+            {0xc2, {0x01}, 1},
+            {0xc3, {0x12}, 1},
+            {0xc4, {0x20}, 1},
+            {0xc6, {0x0f}, 1},
+            {0xd0, {0xa4, 0xa1}, 2},
+            {0xe0, {0xD0,0x04,0x0D,0x11,0x13,0x2B,0x3F,0x54,0x4C,0x18,0x0D,0x0B,0x1F,0x23}, 14},
+            {0xe1, {0xD0,0x04,0x0C,0x11,0x13,0x2C,0x3F,0x44,0x51,0x2F,0x1F,0x1F,0x20,0x23}, 14},
+            {0x21, {}, 0},
+            {0x29, {}, 0},
+    };
+
+private:
+    esp_lcd_i80_bus_handle_t bus_handle = nullptr;
+    esp_lcd_panel_io_handle_t io_handle = nullptr;
+    esp_lcd_panel_handle_t panel_handle = nullptr;
+    lv_disp_drv_t lv_drv = {};
+    lv_disp_t *lv_disp = nullptr;
+};

0db3d9c07d011c8510fa898c66c23fe2ba251632
Jackson Hu <huming2207@gmail.com>
2024-01-23 22:59:44 +1100

    Adapt latest ArduinoJSON

---
+++ main/misc/includes/psram_json_allocator.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <ArduinoJson.hpp>
+#include <esp_heap_caps.h>
+
+class PsRamAllocator : ArduinoJson::Allocator
+{
+    void *allocate(size_t len) override
+    {
+        return heap_caps_malloc(len, MALLOC_CAP_SPIRAM);
+    };
+
+    void deallocate(void *ptr) override
+    {
+        heap_caps_free(ptr);
+    };
+
+    void *reallocate(void *ptr, size_t len) override
+    {
+        return heap_caps_realloc(ptr, len, MALLOC_CAP_SPIRAM);
+    };
+};
